cea5eb72c77f7b87223c749e3700506a0e223e4a
Ahmad Yazid <ahmad.yazid.upnjatim@gmail.com>
2022-08-26 20:51:31 +0700

    navigation bar

--- index.php
+++ index.php
@@ -5,11 +5,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Satisfy' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Tourism Village</title>
 </head>
 
 <body>
+    <nav class="navbar-one flex">
+        <div class="left flex">
+            <div class="logo">
+                <!-- <h1>Brand</h1> -->
+            </div>
+        </div>
+        <div class="right flex">
+            <h1>Travel all over indonesia</h1>
+        </div>
+    </nav>
+
+    <nav class="navbar-second flex">
+        <div></div>
+        <ul class="flex">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#explore">Explore</a></li>
+            <li><a href="#blog">Blog</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+            <li><a href="#login">LOGIN</a></li>
+            <li><a href="#signup">SIGN UP</a></li>
+        </ul>
+    </nav>
 
 </body>
+<!-- </div> -->
 
 </html>
