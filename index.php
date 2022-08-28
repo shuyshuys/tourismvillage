a572342d058bdd1f5f69ff8145e54769d4121598
Ahmad Yazid <ahmad.yazid.upnjatim@gmail.com>
2022-08-28 10:19:29 +0700

    add navbar, home, explore, footer section

--- index.php
+++ index.php
@@ -5,37 +5,107 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href='https://fonts.googleapis.com/css?family=Satisfy' rel='stylesheet'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
-    <link rel="stylesheet" href="./css/style.css">
     <title>Tourism Village</title>
+
+    <!-- font family Satisfy -->
+    <link href='https://fonts.googleapis.com/css?family=Satisfy' rel='stylesheet'>
+
+    <!-- fontawesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <!-- boxicons file link-->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
 </head>
 
 <body>
-    <nav class="navbar-one flex">
-        <div class="left flex">
-            <div class="logo">
-                <!-- <h1>Brand</h1> -->
+    <!-- header section starts -->
+    <header id="home">
+        <!-- navigation one starts -->
+        <nav class="navbar-one flex">
+            <div class="left flex">
+                <div class="logo">
+                </div>
+            </div>
+            <div class="right flex">
+                <h1>Travel all over indonesia</h1>
+            </div>
+        </nav>
+        <!-- navigation one ends -->
+        <!-- navigation second starts -->
+        <nav class="navbar-second flex">
+            <div></div>
+            <ul class="flex">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#explore">Explore</a></li>
+                <li><a href="#blog">Blog</a></li>
+                <li><a href="#contact">Contact Us</a></li>
+                <li><a href="#login">Login</a></li>
+                <li><a href="#signup">Sign Up</a></li>
+            </ul>
+        </nav>
+        <!-- navigation second ends -->
+        <div id="btn-menu" class="fas fa-bars"></div>
+    </header>
+    <!-- header section ends -->
+
+    <section>
+        <div class="text-container">
+            <h1 class="title">indonesia</h1>
+            <p class="subtitle">exploring Indonesia in various villages</p>
+            <a href="#explore" class="btn">Explore<i class="fa-solid fa-arrow-right"></i></a>
+        </div>
+        <!-- <div class="maps"> -->
+        <p align="center" class="maps" id="explore"><br><iframe
+                src="https://www.google.com/maps/d/embed?mid=1bPmLb7V5PliEMUHhTNHmj0Ckxd0WShw&ehbc=2E312F" width="1080"
+                height="640"></iframe></p>
+        <!-- </div> -->
+    </section>
+
+    <!-- footer section starts -->
+    <br>
+    <section class="footer">
+        <br>
+        <div class="box-container">
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="#home"><i class="fas fa-angle-right"></i> home</a>
+                <a href="#explore"><i class="fas fa-angle-right"></i> explore</a>
+                <a href="#blog"><i class="fas fa-angle-right"></i> blog</a>
+                <a href="#contact"><i class="fas fa-angle-right"></i> contact us</a>
+            </div>
+
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="faq.php"><i class="fas fa-angle-right"></i> ask question</a>
+                <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+                <a href="#"><i class="fas fa-angle-right"></i> term of use</a>
+            </div>
+
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="#"><i class="fas fa-phone"></i> +1 (845) 251-5940<a>
+                        <a href="mailto:ivlyns@pm.me"><i class="fas fa-envelope"></i> ivlyns@pm.me<a>
+                                <a href="#"><i class="fas fa-map"></i>Jawa Timur, Indonesia - 134652</a>
+            </div>
+
+            <div class="box">
+                <h3>follow us</h3>
+                <a href="#"><i class="fab fa-facebook"></i> facebook</a>
+                <a href="https://github.com/shuyshuys"><i class="fab fa-github"></i> github</a>
+                <a href="https://instagram.com/garudahacks"><i class="fab fa-instagram"></i> instagram</a>
+                <a href="#"><i class="fab fa-linkedin"></i> linkedin</a>
             </div>
         </div>
-        <div class="right flex">
-            <h1>Travel all over indonesia</h1>
-        </div>
-    </nav>
 
-    <nav class="navbar-second flex">
-        <div></div>
-        <ul class="flex">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#explore">Explore</a></li>
-            <li><a href="#blog">Blog</a></li>
-            <li><a href="#contact">Contact Us</a></li>
-            <li><a href="#login">LOGIN</a></li>
-            <li><a href="#signup">SIGN UP</a></li>
-        </ul>
-    </nav>
+        <div class="credit">created by <span>TourismVillage Admin</span> | all right reserved!</div>
+    </section>
+    <!-- footer section ends -->
 
+    <!-- link to javascript -->
+    <script type="text/javascript" src="./js/style.js"></script>
 </body>
-<!-- </div> -->
 
 </html>
